84945aaae75039761f1a5b7ca3cb20d639a740dc
phptech0000 <php.tech0000@outlook.com>
2022-11-23 19:20:55 -0300

    filter posts for only friends

--- app/Http/Livewire/FetchPosts.php
+++ app/Http/Livewire/FetchPosts.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Friendship;
 use App\Models\Like;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FetchPosts extends Component
@@ -13,8 +16,15 @@ class FetchPosts extends Component
 
     public function render()
     {
+        $currentUser = Auth()->user();
+        $friendships1 = Friendship::where('user_receive', $currentUser->id)->where('friends', true)->get()->pluck('user_id');
+        $friendships2 = Friendship::where('user_id', $currentUser->id)->where('friends', true)->get()->pluck('user_receive');
+
+        // Log::info($friendships1);
+        // Log::info($friendships2);
+        $friends = array_merge($friendships1->toArray(), $friendships2->toArray());
         $posts =
-            Post::with('user')->with('likes')->latest()->get();
+            Post::whereIn('user_id', $friends)->with('user')->with('likes')->latest()->get();
         // dd($posts);
         return view('livewire.fetch-posts', compact('posts'));
     }
