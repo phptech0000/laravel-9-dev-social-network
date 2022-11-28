d141fc15586b7bd60e2dd6dd988f5bd14e021ab3
phptech0000 <php.tech0000@outlook.com>
2022-11-28 15:53:39 -0300

    add logo and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,26 @@
         <main>
             {{ $slot }}
         </main>
+
+        <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Pedro Henrique Lopes</a>.
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Sobre</a>
+                </li>
+                <!-- <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+                </li> -->
+                <li>
+                    <a href="#" class="hover:underline">Contato</a>
+                </li>
+            </ul>
+        </footer>
+
     </div>
 
     @livewireScripts
--- resources/views/livewire/send-message.blade.php
+++ resources/views/livewire/send-message.blade.php
@@ -2,6 +2,6 @@
     <form class="pt-6 flex" wire:submit.prevent="store({{ $chatID }})">
         <!-- <input type="hidden" wire:model="receiverId" value="{{ request()->userID }}"> -->
         <input type="text" class="border w-full p-3 px-4 rounded-md" wire:model="message" placeholder="Message">
-        <button class="bg-purple-500 text-white px-6 py-2">Send</button>
+        <button class="bg-purple-500 text-white px-6 py-2">Enviar</button>
     </form>
 </div>
