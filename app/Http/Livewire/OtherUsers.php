9da822846104c084c54a09c38d71c7265e4a0783
phptech0000 <php.tech0000@outlook.com>
2022-12-01 21:09:03 -0300

    add input to search users

--- app/Http/Livewire/OtherUsers.php
+++ app/Http/Livewire/OtherUsers.php
@@ -10,19 +10,26 @@ use Illuminate\Support\Facades\Log;
 
 class OtherUsers extends Component
 {
+
+    public $search;
+
+    public function updateSearch($value)
+    {
+        info($value);
+    }
     public function render()
     {
         $currentUser = Auth()->user();
-        $friendships1 = Friendship::where('user_receive', $currentUser->id)->where('friends', true)->get()->pluck('user_id');
-        $friendships2 = Friendship::where('user_id', $currentUser->id)->where('friends', true)->get()->pluck('user_receive');
+        $friendships1 = Friendship::where('user_receive',  Auth()->user()->id)->where('friends', true)->get()->pluck('user_id');
+        $friendships2 = Friendship::where('user_id',  Auth()->user()->id)->where('friends', true)->get()->pluck('user_receive');
 
         $friends = array_merge($friendships1->toArray(), $friendships2->toArray());
 
-        $usersFriends = User::whereIn('id', $friends)->get();
-        $outherUsers = User::whereNotIn('id', $friends)->where('id', "!=", $currentUser->id)->get();
+        // $usersFriends = User::whereIn('id', $friends)->get();
+        $outherUsers = User::whereNotIn('id', $friends)->where('id', "!=", Auth()->user()->id)->get();
 
 
 
-        return view('livewire.other-users', compact('usersFriends', 'outherUsers'));
+        return view('livewire.other-users', compact('outherUsers'));
     }
 }
--- resources/views/livewire/other-users.blade.php
+++ resources/views/livewire/other-users.blade.php
@@ -1,22 +1,10 @@
 <div class="mt-8">
-
-    <!-- 
-    <div>
-        <h1 class="text-gray-700 text-2xl">Amigos:</h1>
-
-        @foreach($usersFriends as $user)
-        <div class="flex justify-between bg-gray-100 mt-4 p-4 shadow-lg">
-            <a href="{{ route('user.posts', $user->id) }}" class="text-lg font-bold">{{ $user->username }}</a>
-            <a href="{{ route('messages', $user->id) }}" class="bg-purple-500 text-white rounded p-2">Iniciar Chat</a>
-            <a href="{{ route('make-friendship', $user->id) }}" class="bg-green-300 text-white p-2">Adicionar Amigo</a>
-        </div>
-        @endforeach
-    </div> -->
-
-
     <div class="mt-10">
         <h1 class="text-gray-700 text-2xl">Outros Usuários:</h1>
-
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="search">
+            Pesquise pelo nome:
+        </label>
+        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-white rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="search" type="text" placeholder="pesquisar...." wire:change="updateSearch($event.target.value)">
         @foreach($outherUsers as $user)
         <div class="flex justify-between bg-gray-100 mt-4 p-4 shadow-lg ">
             <a href="{{ route('user.posts', $user->id) }}" class="text-lg font-bold">{{ $user->username }}</a>
@@ -24,7 +12,4 @@
         </div>
         @endforeach
     </div>
-
-
-
 </div>
