a57dc86c1a0c646dc62823a43df14555c27e6a80
phptech0000 <php.tech0000@outlook.com>
2022-12-04 10:41:42 -0300

    change  post migration and model

--- database/migrations/2022_10_14_013801_create_posts_table.php
+++ database/migrations/2022_10_14_013801_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('image')->nullable();
+            $table->string('title');
             $table->text('body');
             $table->foreignId('user_id');
             $table->timestamps();
