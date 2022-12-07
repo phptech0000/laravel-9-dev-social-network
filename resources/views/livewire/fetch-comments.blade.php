3ab9015a0c0a092b3c1eb1db5287ec4b370fc563
phptech0000 <php.tech0000@outlook.com>
2022-12-06 21:42:21 -0300

    instructions for make the feature

--- resources/views/livewire/fetch-comments.blade.php
+++ resources/views/livewire/fetch-comments.blade.php
@@ -57,6 +57,8 @@
                                 </div>
                             </div>
                         </div>
+                        <!-- COLOCAR UM INPUT PARA RESPONDER O COMENTARIO
+                        PEGAR O ID DO COMENTARIO ATUAL E GRAVAR EM UMA TABELA DE RESPOSTAS COM O ID DO COMENTARIO RESPONDIDO, E A RESPOSTA EM SÍ -->
                     </div>
                     <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                         <button wire:click="closeModal" type="button" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Fechar</button>
