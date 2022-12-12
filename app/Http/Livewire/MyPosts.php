3849b49ea0f21f0a4f56446e11600471f42b2eeb
phptech0000 <php.tech0000@outlook.com>
2022-12-12 08:40:42 -0300

    my-posts to livewire component done

--- app/Http/Livewire/MyPosts.php
+++ app/Http/Livewire/MyPosts.php
@@ -3,9 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class MyPosts extends Component
 {
+
+
+    public $posts;
+
+    public function mount()
+    {
+        $this->posts =  Post::where('user_id', Auth()->user()->id)->get();
+    }
     public function render()
     {
         return view('livewire.my-posts');
