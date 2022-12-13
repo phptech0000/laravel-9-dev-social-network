803537a6c8164db895468001f91e3c731fe39b3b
phptech0000 <php.tech0000@outlook.com>
2022-12-12 21:11:10 -0300

    add delete own post function

--- app/Http/Livewire/MyPosts.php
+++ app/Http/Livewire/MyPosts.php
@@ -10,12 +10,34 @@ class MyPosts extends Component
 
 
     public $posts;
-    public $modal = false;
+    public $deleteModal = false;
+    public Post $postDelete;
 
     public function mount()
     {
         $this->posts =  Post::where('user_id', Auth()->user()->id)->get();
     }
+
+
+    public function openModal($postId)
+    {
+        $this->deleteModal = true;
+        $this->postDelete = Post::find($postId);
+    }
+
+    public function closeModal()
+    {
+        $this->deleteModal = false;
+    }
+
+    public function deletePost()
+    {
+        Post::where('id', $this->postDelete->id)->delete();
+        session()->flash('success', 'Post Deletado!');
+        return redirect(route('my.posts'));
+    }
+
+
     public function render()
     {
         return view('livewire.my-posts');
