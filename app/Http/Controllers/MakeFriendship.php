29c6e49b14998d12b86f0019b442ff8d25179e06
phptech0000 <php.tech0000@outlook.com>
2022-12-14 20:59:55 -0300

    add exceptions in controllers

--- app/Http/Controllers/MakeFriendship.php
+++ app/Http/Controllers/MakeFriendship.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\Friendship;
+use Exception;
 
+// use Exception
 class MakeFriendship extends Controller
 {
     public function makeFriendship($userId)
@@ -13,39 +15,49 @@ class MakeFriendship extends Controller
 
         $currentUser = Auth()->user();
 
-        $friendship = Friendship::where('user_id', $currentUser->id)->where('user_receive', $userId)->first();
-        $friendshipTwo = Friendship::where('user_id', $userId)->where('user_receive', $currentUser->id)->first();
+        try {
+            $friendship = Friendship::where('user_id', $currentUser->id)->where('user_receive', $userId)->first();
+            $friendshipTwo = Friendship::where('user_id', $userId)->where('user_receive', $currentUser->id)->first();
 
-        if ($friendship == false && $friendshipTwo == false) {
-            Friendship::create([
-                'user_id' => $currentUser->id,
-                'user_receive' => $userId
-            ]);
-        } elseif ($friendshipTwo) {
-            session()->flash('danger', 'Já existe uma solicitação pendente entre você e esse usuário!');
+            if ($friendship == false && $friendshipTwo == false) {
+                Friendship::create([
+                    'user_id' => $currentUser->id,
+                    'user_receive' => $userId
+                ]);
 
-            return  to_route('other-users.index');
-        } else {
-            session()->flash('danger', 'Solicitação já enviada para esse usuário!');
+                session()->flash('success', 'Solicitação enviada!');
+
+                return  to_route('other-users.index');
+            } elseif ($friendshipTwo) {
+                session()->flash('danger', 'Já existe uma solicitação pendente entre você e esse usuário!');
+
+                return  to_route('other-users.index');
+            } else {
+                session()->flash('danger', 'Solicitação já enviada para esse usuário!');
+
+                return  to_route('other-users.index');
+            }
+        } catch (Exception $e) {
+            session()->flash('danger', 'Não foi possivél  enviar a solicitação!');
 
             return  to_route('other-users.index');
         }
-
-
-        session()->flash('success', 'Solicitação enviada!');
-
-        return  to_route('other-users.index');
     }
 
     public function confirmFriendship($idUser)
     {
 
-        $friendship = Friendship::where('user_receive', Auth()->user()->id)->where('user_id', $idUser)->first();
+        try {
+            $friendship = Friendship::where('user_receive', Auth()->user()->id)->where('user_id', $idUser)->first();
 
-        $friendship->update(['friends' => true]);
+            $friendship->update(['friends' => true]);
 
-        session()->flash('success', 'Solicitação de amizade aceita!');
-        return back();
+            session()->flash('success', 'Solicitação de amizade aceita!');
+            return back();
+        } catch (Exception $e) {
+            session()->flash('danger', 'Não foi possivél aceitar a solicitação!');
+            return back();
+        }
     }
 
     public function MyFriendships()
--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Chat;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -25,23 +26,28 @@ class MessageController extends Controller
             session()->flash('danger', 'Não é possivél entrar em um chat com seu proprio usuário!');
             return back();
         }
-        $situationOne = Chat::where('user_one_id', Auth()->user()->id)->where('user_two_id', $userId)->first();
-        $situationTwo = Chat::where('user_one_id', $userId)->where('user_two_id', Auth()->user()->id)->first();
+        try {
+            $situationOne = Chat::where('user_one_id', Auth()->user()->id)->where('user_two_id', $userId)->first();
+            $situationTwo = Chat::where('user_one_id', $userId)->where('user_two_id', Auth()->user()->id)->first();
 
-        if ($situationOne || $situationTwo) {
+            if ($situationOne || $situationTwo) {
 
-            if ($situationOne) {
-                $chat = Chat::where('id', $situationOne->id)->first();
+                if ($situationOne) {
+                    $chat = Chat::where('id', $situationOne->id)->first();
+                    return view('messages', compact('chat'));
+                }
+                $chat = Chat::where('id', $situationTwo->id)->first();
+                return view('messages', compact('chat'));
+            } else {
+                $chat = Chat::create([
+                    'user_one_id' => Auth()->user()->id,
+                    'user_two_id' => $userId
+                ]);
                 return view('messages', compact('chat'));
             }
-            $chat = Chat::where('id', $situationTwo->id)->first();
-            return view('messages', compact('chat'));
-        } else {
-            $chat = Chat::create([
-                'user_one_id' => Auth()->user()->id,
-                'user_two_id' => $userId
-            ]);
-            return view('messages', compact('chat'));
+        } catch (Exception $e) {
+            session()->flash('danger', 'Não é possivél iniciar o chat!');
+            return back();
         }
     }
 }
