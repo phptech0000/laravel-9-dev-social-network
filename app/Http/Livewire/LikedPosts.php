7728e06a6549a2da1eec907362484a25d7f5cc3c
phptech0000 <php.tech0000@outlook.com>
2022-12-24 21:56:03 -0300

    render posts liked

--- app/Http/Livewire/LikedPosts.php
+++ app/Http/Livewire/LikedPosts.php
@@ -3,11 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Like;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class LikedPosts extends Component
 {
+    public $postsLiked;
+
     public function render()
     {
+        $likes = Like::where('user_id', Auth()->user()->id)->get()->pluck('post_id');
+        $this->postsLiked = Post::whereIn('id', $likes)->get();
         return view('livewire.liked-posts');
     }
 }
