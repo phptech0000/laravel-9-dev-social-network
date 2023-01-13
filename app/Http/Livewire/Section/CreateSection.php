1ff0367854bdcbf4321d333b2f78bdba885e6435
phptech0000 <php.tech0000@outlook.com>
2023-01-12 21:57:06 -0300

    create section

--- app/Http/Livewire/Section/CreateSection.php
+++ app/Http/Livewire/Section/CreateSection.php
@@ -3,9 +3,17 @@
 namespace App\Http\Livewire\Section;
 
 use Livewire\Component;
+use App\Models\Section;
 
 class CreateSection extends Component
 {
+    public $name;
+    public function store()
+    {
+        Section::create(['name' => $this->name]);
+        session()->flash('success', 'sessão criada!');
+        return redirect()->route('section.index');
+    }
     public function render()
     {
         return view('livewire.section.create-section');
--- resources/views/livewire/section/create-section.blade.php
+++ resources/views/livewire/section/create-section.blade.php
@@ -1,3 +1,11 @@
 <div>
     <h1>CRIAR SESSÃO</h1>
+
+    <form wire:submit.prevent="store" class="grid gap-3">
+        <input type="text" wire:model="name" class="border rounded-md p-6 focus:outline-none px-4 w-full" placeholder="Nome da Seção..." id="">
+
+        <div>
+            <button class="px-6 py-4 rounded-md bg-purple-500 text-white">Criar Sessão</button>
+        </div>
+    </form>
 </div>
