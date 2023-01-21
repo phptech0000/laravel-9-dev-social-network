526b9466a7c0078f06c9fe9a2ed75c4fa6ead4cf
phptech0000 <php.tech0000@outlook.com>
2023-01-21 19:28:48 -0300

    begin componente to show posts of section

--- app/Http/Livewire/Section/PostsSection.php
+++ app/Http/Livewire/Section/PostsSection.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class PostsSection extends Component
 {
+    public $section;
+    public function mount($section)
+    {
+        $this->section = $section;
+    }
     public function render()
     {
         return view('livewire.section.posts-section');
--- resources/views/posts-section.blade.php
+++ resources/views/posts-section.blade.php
@@ -10,7 +10,8 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
         <div class="p-6 bg-white border-b border-gray-200">
-          Posts da sessão tal
+          <livewire:section.posts-section :section="$section" />
+
 
         </div>
       </div>
