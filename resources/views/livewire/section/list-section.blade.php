8c5f7d26cf41b8f59807af752609f4a3942b77ab
phptech0000 <php.tech0000@outlook.com>
2023-01-21 19:18:44 -0300

    create links to sections posts

--- resources/views/livewire/section/list-section.blade.php
+++ resources/views/livewire/section/list-section.blade.php
@@ -7,8 +7,7 @@
         @foreach($sections as $section)
         <div class="flex justify-between bg-gray-100 mt-4 p-4 shadow-lg">
             <a href="#" class="text-lg font-bold">{{ $section->name }}</a>
-            <a href="#" class="bg-purple-500 text-white rounded p-2">Ver Posts da Sessão</a>
-
+            <a href="{{ route('section.posts', $section->id) }}" class="bg-purple-500 text-white rounded p-2">Ver Posts da Sessão</a>
         </div>
         @endforeach
         @else
