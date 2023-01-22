f2489562511efca1c5c27e247a56a650f9d4158d
phptech0000 <php.tech0000@outlook.com>
2023-01-22 18:21:30 -0300

    show posts of section done

--- app/Http/Livewire/Section/PostsSection.php
+++ app/Http/Livewire/Section/PostsSection.php
@@ -3,13 +3,16 @@
 namespace App\Http\Livewire\Section;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class PostsSection extends Component
 {
     public $section;
+    public $posts;
     public function mount($section)
     {
         $this->section = $section;
+        $this->posts = Post::where('section', $section->id)->get();
     }
     public function render()
     {
