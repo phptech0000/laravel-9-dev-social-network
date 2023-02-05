8d473c2b69b1085503f3e2d2047ed5ce4076ecd8
phptech0000 <php.tech0000@outlook.com>
2023-02-05 20:29:16 -0300

    add new layout to other users profiles

--- resources/views/other-user-profile.blade.php
+++ resources/views/other-user-profile.blade.php
@@ -9,17 +9,12 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
+          <livewire:other-user-profile />
 
         </div>
       </div>
     </div>
   </div>
 
-  <!-- <div class="py-4 mb-4">
 
-
-
-
-
-  </div> -->
 </x-app-layout>
