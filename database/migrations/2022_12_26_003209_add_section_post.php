a15d008e9e3aa702134b3acc109edb38b68062ec
phptech0000 <php.tech0000@outlook.com>
2023-03-23 19:40:23 -0300

    fix some migrations

--- database/migrations/2022_12_26_003209_add_section_post.php
+++ database/migrations/2022_12_26_003209_add_section_post.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function ($table) {
-            $table->string('section');
+            $table->string('section')->nullable();
         });
     }
 
--- database/migrations/2023_01_25_222819_users_photo.php
+++ database/migrations/2023_01_25_222819_users_photo.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->string('photo');
+            $table->string('photo')->nullable();
         });
     }
 
