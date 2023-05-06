0594b02a4efad34c0cc61cfa1de9d6aadf4c41d6
phptech0000 <php.tech0000@outlook.com>
2023-05-06 17:37:38 -0300

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,12 +11,13 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
         <div class="p-6 bg-white border-b border-gray-200 text-center">
-          <h1 class="text-2xl"><b>ForDevs</b></h1>
-          <p>Uma plataforma desenvolvida com o propósito de contribuir para a comunidade dev.</p>
-          <p>Um local onde as pessoas poderam compartilhar seus conhecimentos com outros usuários, e também ajudar uns aos outros.</p>
+          <h1 class="text-2xl"><b>Energy Channel</b></h1>
+          <p>Uma plataforma criada com o objetivo de conectar a comunidade cientifica e a sociedade civil.</p>
+          <p>Um local onde as pessoas poderam discutir sobre projetos e tecnologias no setor de energias renováveis,<br> bem como compartilhar informações confiáveis sobre o tema.</p>
+          <p>A intenção é incentivar e orientar a todos no uso dos recursos renováveis e como fazê-lo</p>
 
           <div class="flex my-4 justify-between w-4/6 mx-auto">
-            <div>
+            <!-- <div>
 
               <p><b>Desenvolvedor:</b> Pedro Henrique Lopes</p>
             </div>
@@ -24,7 +25,7 @@
 
               <p> <b>Design:</b> Carol</p>
 
-            </div>
+            </div> -->
           </div>
         </div>
 
